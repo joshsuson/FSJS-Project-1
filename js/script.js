63cae887ad84854bfd5b982b8300d815ca05ac9b
Josh Suson <josh.suson@gmail.com>
2019-12-09 18:56:35 -0500

    complete project

--- js/script.js
+++ js/script.js
@@ -11,17 +11,80 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+const quotes = [
+  {
+    quote: 'Get a friend, loser.',
+    source: 'Dwight Shrute',
+    citation: 'Customer Survey',
+    year: 'Season 5 (2008)'
+  },
+  {
+    quote: "I'm about to do something very bold in this job that I've never done before...try",
+    source: 'Jim Halpert',
+    citation: 'Job Fair',
+    season: 'Season 4',
+    year: '2007'
+  },
+  {
+    quote: 'I miss Dwight. Congratulations, universe. You win.',
+    source: 'Jim Halpert',
+    citation: 'The Return',
+    season: 'Season 3',
+    year: '2006'
+  },
+  {
+    quote: "In the end, the greatest snowball isn't a snowball at all. It's fear. Merry Christmas.",
+    source: 'Dwight Shrute',
+    citation: 'Classy Christmas pt. 1',
+    season: 'Season 7',
+    year: '2010'
+  },
+  {
+    quote: 'I knew exactly what to do. But in a much more real sense, I had no idea what to do.',
+    source: 'Michael Scott',
+    citation: 'Stress Relief',
+    season: 'Season 5',
+    year: '2008'
+  }
+];
+
+
+
 
 
 /***
  * `getRandomQuote` function
 ***/
-
+function getRandomQuote() {
+  let quote = quotes[Math.floor(Math.random() * quotes.length)];
+  return quote;
+}
 
 
 /***
  * `printQuote` function
 ***/
+function printQuote() {
+  let randomQuote = getRandomQuote();
+  let quoteHTML = '<p class = "quote">' + randomQuote.quote + '</p> ' +
+                  '<p class = "source">' + randomQuote.source + 
+                      ' <span class = "season">' + randomQuote.season + ':</span> ' +
+                      '<span class = "citation">' + randomQuote.citation + '</span>' +
+                      ' <span class = "year">' + randomQuote.year + '</span>' +
+                      '</p>';
+  let quoteBox = document.getElementById('quote-box');
+  quoteBox.innerHTML = quoteHTML;
+  
+  // Create Random Background
+  let red = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 256);
+  let blue = Math.floor(Math.random() * 256);
+  let randomColor = "rgb("+ red +", "+ green +", "+ blue +")"
+  let body = document.getElementsByTagName('body');
+  body[0].style.background = randomColor;
+}
+printQuote();
+setInterval(printQuote, 7000);
 
 
 
